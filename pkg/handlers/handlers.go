23b3f744ba6a8f850765837da368a854b3884c8e
codeabuu <abdulngui718@gmail.com>
2024-07-03 17:49:23 -0400

    updates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,1 +1,48 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/codeabuu/AWS-Serverless/pkg/user"
+)
+
+var ErrorMethodNotAllowed = "Method not allowed"
+
+type ErrorBody struct {
+	ErroMsg *string `json:"error, omitempty"`
+}
+
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetchUser(email, tableName, dynaClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusOK, result)
+	}
+	result, err := user.FetchUsers(email, tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+	}
+	return apiResponse(http.StatusOK, result)
+}
+
+func CreateUser() (*events.APIGatewayProxyResponse, error) {
+
+}
+
+func UpdateUser() (*events.APIGatewayProxyResponse, error) {
+
+}
+
+func DeleteUser() (*events.APIGatewayProxyResponse, error) {
+
+}
+
+func UnhandledMethod() (*events.APIGatewayProxyResponse, error) {
+	return apiResponse(http.StatusMethodNotAllowed, ErrorMethodNotAllowed)
+}
--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,1 +1,38 @@
 package user
+
+import (
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var ()
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*User){
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			
+		}
+	}
+}
+
+func FetchUsers() {
+
+}
+
+func CreateUser() {
+
+}
+
+func UpdateUser() {
+
+}
+
+func DeleteUser() error {
+
+}
